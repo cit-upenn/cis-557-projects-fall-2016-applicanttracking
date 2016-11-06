d9de8ce08b2180aafe125d9475c76bc411257f8d
chenjr95 <chenjr@seas.upenn.edu>
2016-11-06 15:22:00 -0500

    updated test files

--- applicanttracking/samples/test.rb
+++ applicanttracking/samples/test.rb
@@ -17,13 +17,22 @@ require 'json'
 # puts JSON.parse(response.body)
 
 # sample POST
-uri = URI("http://localhost:3000/users.json")
+# uri = URI("http://cis557-chenjr95.c9users.io:8080/users.json")
+# http = Net::HTTP.new(uri.host, uri.port)
+# uri.query = URI.encode_www_form({
+#   "first_name" => "John",
+#   "last_name" => "Smith"
+# })
+# request = Net::HTTP::Post.new(uri.request_uri)
+# request.initialize_http_header({"Content-Type" => 'application/json'})
+# response = http.request(request)
+# puts response.code
+# puts JSON.parse(response.body)
+
+# sample GET
+uri = URI("http://cis557-chenjr95.c9users.io:8080/users.json")
 http = Net::HTTP.new(uri.host, uri.port)
-uri.query = URI.encode_www_form({
-  "first_name" => "John",
-  "last_name" => "Smith"
-})
-request = Net::HTTP::Post.new(uri.request_uri)
+request = Net::HTTP::Get.new(uri.request_uri)
 request.initialize_http_header({"Content-Type" => 'application/json'})
 response = http.request(request)
 puts response.code
