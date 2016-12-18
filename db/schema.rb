f43567628615a1b9f1225a9339cadcae9e4f6aad
chenjr95 <chenjr@seas.upenn.edu>
2016-12-18 00:11:29 -0400

    merging grace's stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215193230) do
+ActiveRecord::Schema.define(version: 20161217235325) do
 
   create_table "admin_credentials", force: :cascade do |t|
     t.string   "email",                    default: "",      null: false
@@ -50,13 +50,13 @@ ActiveRecord::Schema.define(version: 20161215193230) do
     t.string   "first_name"
     t.string   "middle_name"
     t.string   "last_name"
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-    t.string   "encrypted_password",                 null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0, null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
