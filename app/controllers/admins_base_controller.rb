6be0cbbeb4b432443f258a14c2fa74b48dcd564d
shwang77 <stevenhwang2013@gmail.com>
2016-12-18 18:23:34 -0500

    deleted some tests for feature no longer needed

--- app/controllers/admins_base_controller.rb
+++ app/controllers/admins_base_controller.rb
@@ -37,7 +37,7 @@ class AdminsBaseController < ApplicationController
   # GET /admins/1
   # GET /admins/1.json
   def show
-     puts "called admin show"
+     #puts "called admin show"
      @users = User.all
      users_array = []
      User.all.each do |user|
@@ -116,7 +116,7 @@ class AdminsBaseController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_admin
-      puts "called set_admin"
+      #puts "called set_admin"
       @admin = current_admin_credential
     end
     
