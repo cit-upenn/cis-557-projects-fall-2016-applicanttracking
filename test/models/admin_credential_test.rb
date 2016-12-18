e10ac9b39c3c979bf55f1dbc0e1f87d9408e1c17
chenjr95 <chenjr@seas.upenn.edu>
2016-12-18 00:53:31 -0400

    added back annotations

--- test/models/admin_credential_test.rb
+++ test/models/admin_credential_test.rb
@@ -2,22 +2,31 @@
 #
 # Table name: admin_credentials
 #
-#  id                     :integer          not null, primary key
-#  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
-#  reset_password_token   :string
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  sign_in_count          :integer          default(0), not null
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :string
-#  last_sign_in_ip        :string
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#  provider               :string           default("email"), not null
-#  uid                    :string           default(""), not null
-#  tokens                 :text
+#  id                       :integer          not null, primary key
+#  email                    :string           default(""), not null
+#  encrypted_password       :string           default(""), not null
+#  reset_password_token     :string
+#  reset_password_sent_at   :datetime
+#  remember_created_at      :datetime
+#  sign_in_count            :integer          default(0), not null
+#  current_sign_in_at       :datetime
+#  last_sign_in_at          :datetime
+#  current_sign_in_ip       :string
+#  last_sign_in_ip          :string
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#  provider                 :string           default("email"), not null
+#  uid                      :string           default(""), not null
+#  tokens                   :text
+#  travel_metric            :float            default(0.23)
+#  sport_metric             :float            default(0.03)
+#  personal_projects_metric :float            default(0.23)
+#  voluntary_metric         :float            default(0.08)
+#  achievements_metric      :float            default(0.06)
+#  passion_metric           :float            default(0.06)
+#  study_metric             :float            default(0.11)
+#  work_metric              :float            default(0.11)
+#  self_reflection_metric   :float            default(0.11)
 #
 
 require 'test_helper'
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,6 +2,32 @@
 #
 # Table name: users
 #
+#  id                      :integer          not null, primary key
+#  first_name              :string
+#  middle_name             :string
+#  last_name               :string
+#  dob                     :date
+#  phone                   :integer
+#  email                   :string
+#  street_address          :string
+#  city                    :string
+#  state                   :string
+#  country                 :string
+#  zipcode                 :string
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  submitted               :boolean
+#  user_credential_id      :integer
+#  travel_score            :flaot            default("0")
+#  sport_score             :float            default(0.0)
+#  personal_projects_score :float            default(0.0)
+#  voluntary_score         :float            default(0.0)
+#  achievements_score      :float            default(0.0)
+#  passion_score           :float            default(0.0)
+#  study_score             :float            default(0.0)
+#  work_score              :float            default(0.0)
+#  self_reflection_score   :float            default(0.0)
+#
 
 #  id                 :integer          not null, primary key
 #  first_name         :string
